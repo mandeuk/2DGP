fe3d04ac09e7fabae780ed60b1815fe07df8c710
mandeuk <lih2677@naver.com>
2015-10-23 21:40:27 +0900

    Resource and ControlOption
    1. 배경관련 리소스 추가
    2. 키보드입력관련 코드 수정
    - 한쪽 방향키를 누르고있으면 계속 이동합니다.

--- Miniwatch/main_state.py
+++ Miniwatch/main_state.py
@@ -33,11 +33,32 @@ class Hero:
         self.image = load_image('Resource\Character\Paladin.png')
         self.dir = 1      #0 = 우하, 1 = 하, 2 = 좌하, 3 = 좌, 4 = 좌상, 5 = 상, 6 = 우상, 7 = 우
         self.action = 0     #움직임 판단용
-
+        self.speed = 5
 
     def update(self):
         self.frame = (self.frame + 1) % 4
-        delay(0.15)
+        delay(0.1)
+        if self.action == 1:
+            if self.dir == 0:
+                self.x += self.speed
+                self.y -= self.speed
+            elif self.dir == 1:
+                self.y -= self.speed
+            elif self.dir == 2:
+                self.x += self.speed
+                self.y -= self.speed
+            elif self.dir == 3:
+                self.x -= self.speed
+            elif self.dir == 4:
+                self.x -= self.speed
+                self.y += self.speed
+            elif self.dir == 5:
+                self.y += self.speed
+            elif self.dir == 6:
+                self.x += self.speed
+                self.y += self.speed
+            elif self.dir == 7:
+                self.x += self.speed
 #        self.x += self.dir
 #        if self.x >= 800:
 #            self.dir = -1
@@ -45,7 +66,10 @@ class Hero:
 #            self.dir = 1
 
     def draw(self):
-        self.image.clip_draw(self.frame * 50, self.dir * 50, 50, 50, self.x, self.y)
+        if self.action == 1:
+            self.image.clip_draw(self.frame * 50, self.dir * 50, 50, 50, self.x, self.y)
+        elif self.action == 0:
+            self.image.clip_draw(0, self.dir * 50, 50, 50, self.x, self.y)
 
 
 def enter():
@@ -80,23 +104,60 @@ def handle_events():
             elif event.key == SDLK_DOWN:#하
                 boy.dir = 1
                 boy.y -= 3
+                boy.action = 1
             elif event.key == SDLK_3:#좌하
                 boy.dir = 2
+                boy.action = 1
             elif event.key == SDLK_LEFT:#좌
                 boy.dir = 3
                 boy.x -= 3
+                boy.action = 1
             elif event.key == SDLK_9:#좌상
                 boy.dir = 4
+                boy.action = 1
             elif event.key == SDLK_UP:#상
                 boy.dir = 5
                 boy.y += 3
+                boy.action = 1
             elif event.key == SDLK_7:#우상
                 boy.dir = 6
+                boy.action = 1
             elif event.key == SDLK_RIGHT  :#우
                 boy.dir = 7
                 boy.x += 3
+                boy.action = 1
             elif event.key == SDLK_1:#우하
                 boy.dir = 0
+                boy.action = 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_DOWN:#하
+                boy.dir = 1
+                boy.y -= 3
+                boy.action = 0
+            elif event.key == SDLK_3:#좌하
+                boy.dir = 2
+                boy.action = 0
+            elif event.key == SDLK_LEFT:#좌
+                boy.dir = 3
+                boy.x -= 3
+                boy.action = 0
+            elif event.key == SDLK_9:#좌상
+                boy.dir = 4
+                boy.action = 0
+            elif event.key == SDLK_UP:#상
+                boy.dir = 5
+                boy.y += 3
+                boy.action = 0
+            elif event.key == SDLK_7:#우상
+                boy.dir = 6
+                boy.action = 0
+            elif event.key == SDLK_RIGHT  :#우
+                boy.dir = 7
+                boy.x += 3
+                boy.action = 0
+            elif event.key == SDLK_1:#우하
+                boy.dir = 0
+                boy.action = 0
             Crashcheck() #벽과의 충돌체크
 
 def update():
